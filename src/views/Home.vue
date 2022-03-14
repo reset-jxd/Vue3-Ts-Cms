e57058cb075f11bd77a636318b11090f73a9f2d1
‘jiaxudong’ <1056123142@qq.com>
2022-03-14 09:58:58 +0800

    create project

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Hello {{ $store.state.name }}</h1>
+    <el-button type="primary">哈哈哈哈</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {}
+})
+</script>
